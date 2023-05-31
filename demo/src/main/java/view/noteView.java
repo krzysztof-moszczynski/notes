bb6f9da79a3327e380178f490aa9f71b93575faa
krzysztof <krzysztof.moszczynski@gmail.com>
2023-05-31 15:56:28 +0200

    Note and task view added

---
+++ demo/src/main/java/view/noteView.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+//import model.note;
+
+/**
+ *
+ * @author krzysztof
+ */
+public class noteView {
+    
+    public void printNote(String noteName, String tasks) {
+        System.out.println(noteName);
+        System.out.println(tasks);
+    } 
+}
---
+++ demo/src/main/java/view/taskView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+//import model.task;
+
+
+/**
+ *
+ * @author krzysztof
+ */
+public class taskView {
+    
+    public void printTask(String taskName, String taskDesc) {
+        System.out.println(taskName);
+        System.out.println(taskDesc);
+    }
+    
+    
+}
+ 
