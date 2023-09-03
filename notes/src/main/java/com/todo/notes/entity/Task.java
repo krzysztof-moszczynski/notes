43e75282732bd6e327c3d455b729420e4900b649
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-03 13:59:49 +0200

    POJO Task (put in entity folder for future Spring Boot)

---
+++ notes/src/main/java/com/todo/notes/entity/Task.java
@@ -0,0 +1,18 @@
+package com.todo.notes.entity;
+
+public class Task {
+
+    private String task;
+
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+} 
