59d7a29e36a1de830b3760d74da06636fce6186c
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 05:27:13 +0200

    Unsed imports removed

--- notes/src/main/java/com/todo/notes/NotesApplication.java
+++ notes/src/main/java/com/todo/notes/NotesApplication.java
@@ -3,9 +3,6 @@ package com.todo.notes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.todo.notes.entity.Note;
-import com.todo.notes.entity.Task;
-
 @SpringBootApplication
 public class NotesApplication {
 
