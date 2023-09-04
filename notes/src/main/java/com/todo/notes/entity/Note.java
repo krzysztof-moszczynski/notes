4397470df8d71ac8d0201cb2d65911f6ff43dfd4
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 05:41:59 +0200

    Entities converted to database tables

--- notes/src/main/java/com/todo/notes/entity/Note.java
+++ notes/src/main/java/com/todo/notes/entity/Note.java
@@ -1,5 +1,11 @@
 package com.todo.notes.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +16,15 @@ import lombok.Setter;
 @Getter
 @Setter
 
+@Entity
+@Table(name = "note")
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
     private String title;
 
 }
--- notes/src/main/java/com/todo/notes/entity/Task.java
+++ notes/src/main/java/com/todo/notes/entity/Task.java
@@ -1,5 +1,12 @@
 package com.todo.notes.entity;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +17,18 @@ import lombok.Setter;
 @Getter
 @Setter
 
+@Entity
+@Table(name = "task")
+
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "task")
     private String task;
+    
+
 
 }
