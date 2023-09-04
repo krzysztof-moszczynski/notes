34b28b2a2b525a7bd18229a356414c2d9e26c97a
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 05:02:54 +0200

    lombok annotation added instead of POJO contructor, getters and setters

--- notes/src/main/java/com/todo/notes/entity/Note.java
+++ notes/src/main/java/com/todo/notes/entity/Note.java
@@ -3,59 +3,18 @@ package com.todo.notes.entity;
 import java.util.List;
 import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class Note {
     private String title;
     private List<Task> tasks;
-
-    public Note(String title) {
-        this.title = title;
-        tasks = new ArrayList<>();
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public void addTask(Task task) {
-        tasks.add(task);
-    } 
-
-    public void removeTask(Task task) {
-        tasks.remove(task);
-    }
-
-    public List<Task> getTasks() {
-        return this.tasks;
-    }
-
-    public void setTasks(ArrayList<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " title='" + getTitle() + "'" +
-            ", tasks='" + getTasks() + "'" +
-            "}";
-    }
-
-    /*
-    @Override
-    public String toString() {
-        String result = "" ;
-        result += title + "\n";;
-
-        for(Task t: tasks) {
-            result += t.getTask() + "\n";
-        }
-
-        return result;
-    }
-    */
 
 }
