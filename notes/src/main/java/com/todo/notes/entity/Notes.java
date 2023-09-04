d8b352e3f6ecbf01f24f13963753a668d2c33e7b
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 04:50:09 +0200

    Notes contains Note objects (will be replaced by repository)

---
+++ notes/src/main/java/com/todo/notes/entity/Notes.java
@@ -0,0 +1,17 @@
+package com.todo.notes.entity;
+
+import java.util.ArrayList;
+
+public class Notes {
+
+    private ArrayList<Note> notes;
+
+    public ArrayList<Note> getNotes() {
+        return this.notes;
+    }
+
+    public void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+    
+}
