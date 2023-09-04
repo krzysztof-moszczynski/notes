de700962e376b2e8061e9fb5a286e64545b47e6c
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 05:09:55 +0200

    Note simplified to one title field. The 'container role' will be met using database relations (OneToMany - i.e. one note contains many tasks)

--- notes/src/main/java/com/todo/notes/entity/Note.java
+++ notes/src/main/java/com/todo/notes/entity/Note.java
@@ -1,8 +1,5 @@
 package com.todo.notes.entity;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,5 @@ import lombok.Setter;
 
 public class Note {
     private String title;
-    private List<Task> tasks;
 
 }
