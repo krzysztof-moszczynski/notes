24a6289bdf1ceb8040b6367513e26eead2229d66
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 06:14:03 +0200

    Injecting taks- and noteRepositories

--- notes/src/main/java/com/todo/notes/NotesApplication.java
+++ notes/src/main/java/com/todo/notes/NotesApplication.java
@@ -1,10 +1,20 @@
 package com.todo.notes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.todo.notes.repository.NoteRepository;
+import com.todo.notes.repository.TaskRepository;
+
 @SpringBootApplication
 public class NotesApplication {
+
+	@Autowired
+	TaskRepository taskRepository;
+
+	@Autowired
+	NoteRepository noteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
