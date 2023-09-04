50ba27454c0cc38f8d93838ed4e62d050304cce2
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 04:48:20 +0200

    Constructor, getters and setters added to perform simple test (adding task to note and printing note)

--- notes/src/main/java/com/todo/notes/entity/Note.java
+++ notes/src/main/java/com/todo/notes/entity/Note.java
@@ -1,10 +1,16 @@
 package com.todo.notes.entity;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class Note {
     private String title;
-    private ArrayList<Task> tasks;
+    private List<Task> tasks;
+
+    public Note(String title) {
+        this.title = title;
+        tasks = new ArrayList<>();
+    }
 
     public String getTitle() {
         return title;
@@ -21,4 +27,35 @@ public class Note {
     public void removeTask(Task task) {
         tasks.remove(task);
     }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " title='" + getTitle() + "'" +
+            ", tasks='" + getTasks() + "'" +
+            "}";
+    }
+
+    /*
+    @Override
+    public String toString() {
+        String result = "" ;
+        result += title + "\n";;
+
+        for(Task t: tasks) {
+            result += t.getTask() + "\n";
+        }
+
+        return result;
+    }
+    */
+
 }
