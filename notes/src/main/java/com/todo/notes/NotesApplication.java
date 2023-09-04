71447f21e713b0af631ef10637eb929d7a00f8d0
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-04 05:04:15 +0200

    Simple test removed

--- notes/src/main/java/com/todo/notes/NotesApplication.java
+++ notes/src/main/java/com/todo/notes/NotesApplication.java
@@ -11,18 +11,7 @@ public class NotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
-
-	    Note note1 = new Note("My first note");
-
-	    Task task1 = new Task("Go to gym");
-	    Task task2 = new Task("Go to dentist");
-	    Task task3 = new Task("Go to hell");
-
-		note1.addTask(task1);
-		note1.addTask(task2);
-		note1.addTask(task3);
 	
-		System.out.println(note1);
 	}
 
 }
