95319760718caf8df0b0b5e7bf9fb68dde3f496a
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-06 08:27:46 +0200

    Unused import Task removed

--- notes/src/main/java/com/todo/notes/service/NoteService.java
+++ notes/src/main/java/com/todo/notes/service/NoteService.java
@@ -3,7 +3,6 @@ package com.todo.notes.service;
 import java.util.List;
 
 import com.todo.notes.entity.Note;
-import com.todo.notes.entity.Task;
 
 public interface NoteService {
     Note getNote(Long id);   
