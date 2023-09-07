4de659d1dd09d23f96d4b9edf812f29216e61507
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-07 04:16:45 +0200

    Implementation of CommandLineRunner to add note and some tasks for simple test

--- notes/src/main/java/com/todo/notes/NotesApplication.java
+++ notes/src/main/java/com/todo/notes/NotesApplication.java
@@ -1,14 +1,17 @@
 package com.todo.notes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.todo.notes.entity.Note;
+import com.todo.notes.entity.Task;
 import com.todo.notes.repository.NoteRepository;
 import com.todo.notes.repository.TaskRepository;
 
 @SpringBootApplication
-public class NotesApplication {
+public class NotesApplication implements CommandLineRunner{
 
 	@Autowired
 	TaskRepository taskRepository;
@@ -17,8 +20,24 @@ public class NotesApplication {
 	NoteRepository noteRepository;
 
 	public static void main(String[] args) {
+		//SpringApplication.run(NotesApplication.class, args);
 		SpringApplication.run(NotesApplication.class, args);
-	
+	}
+
+	@Override
+	public void run(String ...args) throws Exception{
+        Note note = new Note("note1");
+		noteRepository.save(note);
+
+		Task[] tasks = new Task[] {
+            new Task("go to gym"),
+            new Task("go to uni"),
+            new Task("go to hell")
+		};
+
+		for(Task task : tasks) {
+			taskRepository.save(task);
+		}
 	}
 
 }
