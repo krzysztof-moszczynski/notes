a6658d73cc365ff6c7dec7405ec6d8469e1223b0
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-07 05:13:09 +0200

    Remaining CRUD methods added

--- notes/src/main/java/com/todo/notes/web/TaskController.java
+++ notes/src/main/java/com/todo/notes/web/TaskController.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todo.notes.entity.Task;
 import com.todo.notes.service.TaskService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -26,11 +30,21 @@ public class TaskController {
         return new ResponseEntity<>(taskService.getTasks(), HttpStatus.OK);
     }
 
-
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable Long id) {
         return new ResponseEntity<>(taskService.getTask(id), HttpStatus.OK);
     }
+    
+    @PostMapping("")
+    public ResponseEntity<Task> createTask(@Valid @RequestBody Task task) {
+        return new ResponseEntity<>(taskService.saveTask(task), HttpStatus.CREATED);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
     
 }
