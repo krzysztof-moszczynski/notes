bd0c826f0704a16cd043f579c04eb5c3ecc29fad
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-07 04:12:26 +0200

    Both entities modified for ManyToMany joinTable note_task

--- notes/src/main/java/com/todo/notes/entity/Note.java
+++ notes/src/main/java/com/todo/notes/entity/Note.java
@@ -2,20 +2,29 @@ package com.todo.notes.entity;
 
 import java.util.List;
 
+import org.aspectj.lang.annotation.RequiredTypes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -26,11 +35,20 @@ public class Note {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @NotBlank
+    @NonNull
     @Column(name = "title")
     private String title;
 
-    @OneToMany(mappedBy = "note", cascade = CascadeType.ALL)
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+        name = "note_task",
+        joinColumns = @JoinColumn(name = "note_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "task_id", referencedColumnName = "id")
+    )
     private List<Task> tasks;
 }
--- notes/src/main/java/com/todo/notes/entity/Task.java
+++ notes/src/main/java/com/todo/notes/entity/Task.java
@@ -1,23 +1,32 @@
 package com.todo.notes.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-
 @Entity
 @Table(name = "task")
 
@@ -25,14 +34,20 @@ public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
-    @Column(name = "activity")
+    @NotBlank
+    @NonNull
+    @Column(name = "activity", nullable = false)
     private String activity;
     
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "note_id", referencedColumnName = "id")
-    private Note note;
-
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+        name = "note_task",
+        joinColumns = @JoinColumn(name = "task_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "note_id", referencedColumnName = "id")
+    )
+    private List<Note> notes;
 
 }
