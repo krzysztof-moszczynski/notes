e22f159673062c6b0f53c0d8c829fa401f65bd36
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-08 19:03:46 +0200

    Basic template for integrity test added

--- notes/src/test/java/com/todo/notes/NotesApplicationTests.java
+++ notes/src/test/java/com/todo/notes/NotesApplicationTests.java
@@ -1,13 +1,40 @@
 package com.todo.notes;
 
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class NotesApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
+	}
+
+	@Test
+	public void testGetTask() throws Exception{
+		RequestBuilder request = MockMvcRequestBuilders.get("/task/1");
+
+		mockMvc.perform(request)
+			.andExpect(status().is2xxSuccessful()
+			//.andExpect(model().name(put the name))
+			//.andExpect(view().attributeExists(put the attribute));
+			);
+
 	}
 
 }
