0e8cefcd9706c3b7b430008839c59ea6be59e48d
krzyszto-moszczynski <krzysztof.moszczynski@gmail.com>
2023-09-19 20:23:29 +0200

    Comments added

--- notes/src/main/java/com/todo/notes/ApplicationExceptionHandler.java
+++ notes/src/main/java/com/todo/notes/ApplicationExceptionHandler.java
@@ -21,28 +21,33 @@ import com.todo.notes.exception.NoteNotFoundException;
 import com.todo.notes.exception.TaskNotFoundException;
 import com.todo.notes.exception.TaskNotInNoteException;
 
+// This class is used to handle exceptions thrown by the application
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({TaskNotFoundException.class, NoteNotFoundException.class, TaskNotInNoteException.class})
+    // This method is used to handle resource not found exceptions
     public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
         ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));  
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
+    // This method is used to handle empty result data access exceptions
     public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex) {
         ErrorResponse error = new ErrorResponse(Arrays.asList("Cannot delete non-existing resource"));  
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
+    // This method is used to handle data integrity violations
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
         ErrorResponse error = new ErrorResponse(Arrays.asList("Data Integrity Violation: we cannot process your request."));  
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @Override 
+    // This method is used to handle validation errors
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         List<String> errors = new ArrayList<>();
--- notes/src/main/java/com/todo/notes/NotesApplication.java
+++ notes/src/main/java/com/todo/notes/NotesApplication.java
@@ -10,7 +10,10 @@ import com.todo.notes.entity.Task;
 import com.todo.notes.repository.NoteRepository;
 import com.todo.notes.repository.TaskRepository;
 
+//Main class of the application
 @SpringBootApplication
+//CommandLineRunner interface implementation used to populate
+//exemplary data 
 public class NotesApplication implements CommandLineRunner{
 
 	@Autowired
@@ -24,24 +27,27 @@ public class NotesApplication implements CommandLineRunner{
 	}
 
 	@Override
-    //CommnadLineRunner interface implementation used to populate 
-    // exemplary data below
+    //Data population
 	public void run(String ...args) throws Exception{
+        //list of notes
 		Note[] notes = new Note[] {
         new Note ("at uni"),
         new Note ("at home")
 		};
 
+        //saving notes to the repository 
 		for (Note note: notes) {
 			noteRepository.save(note);
 		}
 
+        //list of tasks
 		Task[] tasks = new Task[] {
             new Task("go to library"),
             new Task("workout"),
             new Task("CS50 certificate")
 		};
 
+        //saving tasks to the repository
 		for(Task task : tasks) {
 			taskRepository.save(task);
 		}
